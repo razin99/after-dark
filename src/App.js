aeff4b9e9a3f0b41c7c93bdfab355d48d256a86d
Razin Idzuddin <razin99@gmail.com>
2020-10-27 23:51:05 +1100

    basic text field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+
+class MakePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Make a confession..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
-    <div><p>Test</p></div>
+    <div>
+      <p>Test</p>
+      <MakePost />
+    </div>
   );
 }
 
