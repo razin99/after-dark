6c4f3e5960f6d409846057b9668e6ab38b0ef08e
Razin Idzuddin <razin99@gmail.com>
2020-11-07 00:11:01 +1100

    Modal pops up after submitting a post

--- src/components/ConfessionForm.js
+++ src/components/ConfessionForm.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Modal } from "react-bootstrap";
 
 export class ConfessionForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       text: "",
+      code: "",
+      show: false,
     };
-
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
   }
 
   handleTextChange(event) {
@@ -21,46 +25,60 @@ export class ConfessionForm extends Component {
   }
 
   handleSubmit(event) {
-    let code = "";
     event.preventDefault();
     axios
       .post("http://localhost:5000/posts/add", {
         text: this.state.text,
       })
       .then((response) => {
-        code = response.data.code;
+        this.setState({
+          code: response.data.code,
+          show: true,
+        });
+        console.log(this.state.code)
       })
       .catch((err) => {
         console.log(err);
       });
     console.log(this.state.text);
-    this.setState({
-      text: "",
-    });
-    console.log(code)
+    this.setState({ text: "" });
   }
 
   handleReset(event) {
     event.preventDefault();
-    this.setState({
-      text: "",
-    });
+    this.setState({ text: "" });
   }
+
+  handleClose() { this.setState({ show: false }) }
+
+  handleShow() { this.setState({ show: true }) }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
-        <textarea
-          type="text"
-          placeholder="Make a confession..."
-          value={this.state.text}
-          onChange={this.handleTextChange}
-        />
-        <div>
-          <button type="submit">Submit!</button>
-          <button type="reset">Reset!</button>
-        </div>
-      </form>
+      <>
+        <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
+          <textarea
+            type="text"
+            placeholder="Make a confession..."
+            value={this.state.text}
+            onChange={this.handleTextChange}
+          />
+          <div>
+            <button type="submit">Submit!</button>
+            <button type="reset">Reset!</button>
+          </div>
+        </form>
+        <Modal
+          show={this.state.show}
+          onHide={this.handleClose}
+          animation={true}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Callback code: {this.state.code}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Use this code if you want to remove your post</Modal.Body>
+        </Modal>
+      </>
     );
   }
 }
