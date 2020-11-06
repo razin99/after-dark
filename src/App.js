67a095c1c4b41ec46ecc39f766f8ce18f227e2f1
Razin Idzuddin <razin99@gmail.com>
2020-11-06 21:46:56 +1100

    Connected frontend to backend for getting all posts

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import {
   Route
 } from "react-router-dom";
 
-const POSTS = [
-  { text: "abcd", date: "2020-10-23T05:19:27.020+00:00", _id: "5f9261" },
-  { text: "efgh", date: "2020-10-24T05:19:27.020+00:00", _id: "5f9262" },
-  { text: "ijkl", date: "2020-10-25T05:19:27.020+00:00", _id: "5f9263" },
-  { text: "mnop", date: "2020-10-26T05:19:27.020+00:00", _id: "5f9264" },
-  { text: "qrst", date: "2020-10-27T05:19:27.020+00:00", _id: "5f9265" },
-];
-
 
 function App() {
   return (
@@ -25,7 +17,7 @@ function App() {
           <ConfessionForm />
         </Route>
         <Route exact path="/posts">
-          <AllPosts posts={POSTS} />
+          <AllPosts />
         </Route>
       </Router>
     </div>
--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,10 +1,32 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 export class AllPosts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/posts/all")
+      .then((response) => {
+        const all_posts = response.data;
+        this.setState({
+          posts: all_posts,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     const all = [];
 
-    this.props.posts.forEach((post) => {
+    this.state.posts.forEach((post) => {
       let date = new Date(Date.parse(post.date));
       all.push(
         <tr key={post._id}>
